0c7f8cee04ea5501aaedd13093e28c35ad7e4e6b
colinhoar <colin@hoarfamily.org>
2024-07-08 14:37:57 -0400

    House Exterior to Interior Transition
    Collision boxes needed to be added to the full interior of the house.

---
+++ HousetoExterior.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+var entered = false
+
+func _on_body_entered(body: CharacterBody3D) -> void:
+	entered = true
+
+func _on_body_exited(body: CharacterBody3D) -> void:
+	entered = false
+
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("door"):
+			get_tree().change_scene_to_file("res://node_3d.tscn")
