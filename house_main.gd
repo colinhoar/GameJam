2acfee3fdce371853346a66aea341b5fd5859083
colinhoar <colin@hoarfamily.org>
2024-07-09 18:31:18 -0400

    Proper Transition House

--- house_main.gd
+++ house_main.gd
@@ -3,6 +3,11 @@ extends Node3D
 @onready var pause_menu = $PauseMenu
 var paused = false
 
+func _ready() -> void:
+	if Global.testNum == 3:
+		$Player.translate(Vector3(-1.5, 0, 7))
+	Global.testNum = 1
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
--- main.gd
+++ main.gd
@@ -3,6 +3,13 @@ extends Node3D
 @onready var pause_menu = $PauseMenu
 var paused = false
 
+func _ready() -> void:
+	if Global.testNum == 1:
+		$Player.translate(Vector3(32.632, 0, -42.28))
+	if Global.testNum == 2:
+		$Player.translate(Vector3(13.217, 0, 32.645))
+	Global.testNum = 3
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		$Player/Rain.visible = !$Player/Rain.visible
