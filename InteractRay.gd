304d9e36573217a3438308a3b08ea7ac96cfe283
colinhoar <colin@hoarfamily.org>
2024-07-10 14:35:20 -0400

    Cow and Sheep Petting

--- InteractRay.gd
+++ InteractRay.gd
@@ -10,3 +10,6 @@ func _physics_process(delta):
 		
 		if collider is Interactable:
 			prompt.text = collider.prompt_message
+			
+			if Input.is_action_just_pressed("pet"):
+				collider.interact(owner)
--- interactable.gd
+++ interactable.gd
@@ -1,4 +1,9 @@
 extends CollisionObject3D
 class_name Interactable
 
+signal interacted(body)
+
 @export var prompt_message = "Interact"
+
+func interact(body):
+	interacted.emit(body)
